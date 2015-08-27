ce3098a993543f0e0fcc54c39f58c07acc1ae299
Dan Tong <tongvtdan@outlook.com>
2015-08-27 13:25:33 +0700

    Add code
    Init commit

---
+++ Rotten Tomatoes/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Rotten Tomatoes
+//
+//  Created by Dan Tong on 8/27/15.
+//  Copyright (c) 2015 DanTong. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Rotten Tomatoes/MoviesVC.swift
@@ -0,0 +1,60 @@
+//
+//  MoviesVC.swift
+//  Rotten Tomatoes
+//
+//  Created by Dan Tong on 8/27/15.
+//  Copyright (c) 2015 DanTong. All rights reserved.
+//
+
+import UIKit
+
+class MoviesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var moviesTableView: UITableView!
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Movies list"
+        // Do any additional setup after loading the view.
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                    let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+//            println(json)
+            if let json = json {
+                self.movies = json["movies"] as? [NSDictionary]
+                self.moviesTableView.reloadData()
+            }
+            
+        }
+        moviesTableView.dataSource = self
+        moviesTableView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = self.movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        let movie = movies![indexPath.row]
+        cell.titleLabel.text = movie["title"] as? String
+        cell.synopsisLabel.text = movie["synopsis"] as? String
+        let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail")as! String)!
+        cell.posterImage.setImageWithURL(url)
+        return cell
+     }
+    
+}
